80ab80bb70e544d8b8418fcd837118f306931855
RovanLab <rovanlab@gmail.com>
2018-01-25 11:17:56 +0530

    first boot

---
+++ js/common.js
@@ -0,0 +1,33 @@
+// Mobile Menu
+if($(window).width() <= 991){
+ $("#menu").mmenu({
+   "offCanvas": {
+     "position": "right"
+   }
+ });
+}
+
+ // Header Fixed   
+ $(window).scroll(function () {
+  var height = $(window).scrollTop();
+  if (height >100) {
+    $('header').addClass('header-fixed');
+  }
+  else if (height == 0) {
+    $('header').removeClass('header-fixed');
+  }
+});  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
