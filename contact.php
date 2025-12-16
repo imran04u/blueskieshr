8993b407b58861e50fd9c1d66f745c1614963c14
Imran <imran04u@yahoo.com>
2025-12-16 22:38:44 +0530

    update

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 
 // configure
 $from = 'Demo contact form <hr@blueskieshr.com>';
-$sendTo = 'info@blueskieshr.com'; // Add Your Email
+$sendTo = 'hr@blueskieshr.com'; // Add Your Email
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'contact' => 'Contact', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in the email
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
